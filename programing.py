817d6e89cfaa66733ab5627f00d4a64ec3b77fd2
sravan123-cod <mohannag@ gmail.com>
2025-08-01 17:34:32 +0530

    programming

---
+++ programing.py
@@ -0,0 +1,8 @@
+name=input("what is your name")
+print("my name is",name)
+
+age=int (input("what is your age"))
+print("i am ",age)
+
+school=input("what is the name of your school")
+print("my school name is",school)
