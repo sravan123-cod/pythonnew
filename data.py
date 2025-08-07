4993f00117bd8e917fa862747b7bcd1e09e0b271
sravan123-cod <mohannag@ gmail.com>
2025-08-07 17:34:12 +0530

    data

---
+++ data.py
@@ -0,0 +1,15 @@
+name="mohit"
+print("My name is",name)
+print("data type of name is",type(name))
+
+age=11
+print("my age is",age)
+print("data type is",type(age))
+
+number=234.56
+print("number is,number")
+print("data type is",type(number))
+
+isstudent=True
+print("student is",isstudent)
+print("data type is",type(isstudent))
---
+++ string.py
@@ -0,0 +1,4 @@
+word=input("anime dimension")
+a=word[:: -1 ]
+b=a
+print(b)
